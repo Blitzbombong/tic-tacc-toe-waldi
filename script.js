38a516898d92e2f1c33eab4cebc2634741576f35
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2023-06-05 17:26:23 +0200

    added functio render()

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 let fiels = [
     null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
+  'circle',
+  'circle',
+  'circle',
+  null,
+  null,
+  'cross',
+  'cross',
+  null,
 ];
+
+function init(){
+    render();
+}
+
+// render function
+function render(fields) {
+    const content = document.getElementById('content');
+    const table = document.createElement('table');
+
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 3; j++) {
+        const cell = document.createElement('td');
+        const fieldValue = fiels[i * 3 + j];
+        if (fieldValue) {
+            cell.textContent = fieldValue === 'circle' ? 'o' : 'x';
+        }
+        row.appendChild(cell);
+    }
+        table.appendChild(row);
+    }
+
+    content.innerHTML = '';
+    content.appendChild(table);
+}
+
